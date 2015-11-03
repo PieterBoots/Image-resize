576079fb096614ede2ba1fdb306dc2d921d88086
Pieter Boots <Pieterboots@zonnet.nl>
2015-11-03 22:16:35 +0100

    Create resize.cs

---
+++ resize.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Resize
+{
+
+  public static int classifyX(int[] data)
+  {
+    double average = 0;
+    for (int d = 0; d < data.Length; d++)
+    {
+      average = average + data[d];
+    }
+
+    average = average / data.Length;
+
+    int a1 = 0;
+
+    for (int d = 0; d < data.Length; d++)
+    {
+      int twos = 1 << d;
+      int v = (data[d] > average) ? twos : 0;
+      a1 = a1 + v;
+    }
+
+    return a1;
+  }
+
+}
